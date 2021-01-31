6d94aaac00fe5c400062c30afe9097e825fb55b4
Veronica Grady <veronicagrady@yahoo.com>
2021-01-30 23:39:43 -0700

    first commit

---
+++ Loopsweek2HW.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class Loopsweek2HW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		int i = 0;
+		while (i <= 100) {
+			System.out.println(i);
+			i = i + 2;
+		}		
+	}
+}
+
+
